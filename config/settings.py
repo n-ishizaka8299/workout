fa2680eb6d0c79dbeef9f3c2a82aa9a6442ee76f
n-ishizaka8299 <n.ishizaka8299@gmail.com>
2021-05-02 18:19:37 +0900

    workconfig setting

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'workout',
+    'workout.apps.WorkoutConfig',
 ]
 
 # WHITENOISE_USE_FINDERS = True
