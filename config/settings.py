3c8b39ece68e2399ff2c5f2ee2e54b220b223919
n-ishizaka8299 <n.ishizaka8299@gmail.com>
2021-05-02 19:31:36 +0900

    change procfile

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'workout.apps.WorkoutConfig',
+    'workout',
 ]
 
 # WHITENOISE_USE_FINDERS = True
