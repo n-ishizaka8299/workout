8c74710cf1f89f9088630b7364a70e185e2c28ef
n-ishizaka8299 <n.ishizaka8299@gmail.com>
2021-05-02 18:17:13 +0900

    workout change setting

--- config/settings.py
+++ config/settings.py
@@ -32,17 +32,17 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
+    # 'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'workout.apps.WorkoutConfig',
+    'workout',
 ]
 
-WHITENOISE_USE_FINDERS = True
+# WHITENOISE_USE_FINDERS = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
